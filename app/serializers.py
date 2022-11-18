3900f542afbfa166e48c9245bb4a324805232b28
Piyush Singh <piyush.singh@bonamisoftware.com>
2022-11-18 09:50:02 +0530

    Revert "New Project"
    This reverts commit 8fe0e49f43d8ceb147a098caa7654c6380b27872.

--- app/serializers.py
+++
@@ -1,7 +0,0 @@
-from  rest_framework import serializers
-from .models import Song
-
-class SongsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Song
-        fields = '__all__'
--- app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SongsView
-urlpatterns = [
-    path('media/',SongsView.as_view()),
-    path('media/<int:id>',SongsView.as_view())
-]
